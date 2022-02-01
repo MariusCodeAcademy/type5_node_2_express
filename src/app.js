cecf3db8ff229f1f71337f8bae79858a43b59427
Marius <marius@example.com>
2022-02-01 14:42:29 +0200

    http vanila

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
-const functions = require('./functions');
+const http = require('http'); // Importuojame http į mūsų JS kodą
 
-console.log('app.js');
+// O štai apačioje sukuriame serverį, kuris priims ir paduos informaciją.
+// Kol kas nieko naudingo nepasiimame ir nepaduodam, bet kodo - jau nemažai.
 
-const rez = functions.makeCamelCase('Ar veikia Camel Case tikrai?');
-
-console.log('rez ===', rez);
+http
+  .createServer((request, response) => {
+    const { headers, method, url } = request;
+    let body = [];
+    request
+      .on('error', (err) => {
+        console.error(err);
+      })
+      .on('data', (chunk) => {
+        body.push(chunk);
+      })
+      .on('end', () => {
+        body = Buffer.concat(body).toString();
+        console.log('body ===', body);
+      });
+  })
+  .listen(8080); // Activates this server, listening on port 8080.
